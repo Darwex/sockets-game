66c862963d3994ab1df6cc288e9f9c8ceba1c3f6
Vrsek, David <david.vrsek@merck.com>
2018-09-01 20:23:30 +0200

    Rendering clouds and ground

--- client/client.js
+++ client/client.js
@@ -4,7 +4,7 @@
 // G - Ground
 
 const worldMap = [
-  ['-', '-', '-', '-', '-', 'C', '-', '-', '-', '-', 'C', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'C', 'C', 'C', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'C'],
+  ['-', '-', '-', '-', '-', 'C', '-', '-', '-', '-', 'C', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'C', '-', '-', '-', '-', '-', '-', '-', '-', 'C', '-', '-', '-', '-', '-'],
   ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
   ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
   ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
@@ -14,17 +14,39 @@ const worldMap = [
   ['G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G']
 ]
 
+const assets = [
+  {
+    name: 'ground',
+    id: 'ground',
+    uri: 'https://s-media-cache-ak0.pinimg.com/236x/78/c8/d1/78c8d1962cd9ade2be59a35fdd9c8c1e.jpg'
+  },
+  {
+    name: 'background',
+    id: 'background',
+    uri: 'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/yZhWZV2/cartoon-animation-of-a-blue-sky_ekh3vs5gg__F0000.png'
+  },
+  {
+    name: 'cloud',
+    id: 'cloud',
+    uri: 'http://www.clipartsfree.net/vector/medium/mcol_cloud_Clip_Art.png'
+  }
+]
+
 const Utils = () => {
 
-  const intersect = (a, b) => (
-    a.x < b.x + b.width &&
-    a.x + a.width > b.x &&
-    a.y < b.y + b.height &&
-    a.height + a.y > b.y
+  const intersect = (a, b, offset = 0) => (
+    a.x < b.x + b.width + offset &&
+    a.x + a.width  + offset > b.x &&
+    a.y < b.y + b.height  + offset &&
+    a.height + offset + a.y > b.y
   )
 
+  // In case that block would be 1px off visible
+  const viewIntersect = (a, b) => intersect(a, b, 1)
+
   return {
-    intersect
+    intersect,
+    viewIntersect
   }
 }
 
@@ -34,40 +56,50 @@ const Map = (canvas = Canvas()
     x: 0,
     y: 0,
     width: canvas.getDimensions().width,
-    height: canvas.getDimensions().height
+    height: canvas.getDimensions().height,
+    viewMaxX: 0
   }
 
   canvas.renderViewCoordsBox(this.view)
 
   const moveView = step => {
-    this.view.x = (this.view.x + step > 0) ? this.view.x + step : 0
+    if ((this.view.x + this.view.width + step) > this.view.viewMaxX) this.view.x = this.view.viewMaxX - this.view.width
+    else if (this.view.x + step < 0) this.view.x = 0
+    else this.view.x += step
+
     canvas.renderBackground()
     canvas.renderViewCoordsBox(this.view)
     canvas.renderView(this.parsedMap, this.view)
   }
 
-  const moveViewRight = () => moveView(30)
+  const getSizeMultiplicator = () => (1 + Math.random())
+
+  const moveViewRight = () => moveView(50)
   const moveViewLeft = () => moveView(-30)
+
 
   const parseWorldMap = (worldMap = []) => {
     const mapXBlockCount = worldMap[0] ? worldMap[0].length : 0
     const mapYBlockCount = worldMap.length
     const canvasDimensions = canvas.getDimensions()
 
-    const blockWidth = 200
-    const blockHeight = 200
+    const blockWidth = (canvasDimensions.height / 5)
+    const blockHeight = (canvasDimensions.height / mapYBlockCount)
     const parsedMap = []
     for (let i = 0; i < mapYBlockCount; i++) {
       for (let j = 0; j < mapXBlockCount; j++) {
         const currentBlock = worldMap[i][j]
+
+        const sizeMultiplicator = getSizeMultiplicator()
         const parsedBlock = {
           blockType: currentBlock,
           x: (blockWidth * j),
-          y: (blockHeight + blockHeight * i),
-          width: blockWidth,
-          height: blockHeight
+          y: (blockHeight * i),
+          width: (currentBlock  === 'C') ? blockWidth * sizeMultiplicator : blockWidth,
+          height: (currentBlock  === 'C') ? blockHeight * sizeMultiplicator : blockHeight
         }
         parsedMap.push(parsedBlock)
+        this.view.viewMaxX = Math.max(this.view.viewMaxX, (parsedBlock.x + parsedBlock.width))
       }
     }
     return parsedMap
@@ -75,7 +107,6 @@ const Map = (canvas = Canvas()
 
   this.parsedMap = parseWorldMap(worldMap)
   canvas.renderView(this.parsedMap, this.view)
-
 
   return {
     moveViewRight,
@@ -93,17 +124,29 @@ const Canvas = (utils = Utils()) => {
     canvas.height = window.innerHeight
     renderBackground()
   }
-  const renderBlock = (block, view) => {
-    ctx.fillStyle = 'red'
-    ctx.font = `50px Verdana`
-    ctx.fillText(block.blockType, block.x - view.x, block.y)
+
+  const drawImage = (assetId, x, y, width, height) => {
+    const image = document.getElementById(assetId)
+    ctx.drawImage(image, x, y, width, height)
   }
 
+  const renderBlock = (block, view) => {
+    switch (block.blockType) {
+    case 'G':
+      drawImage('ground', block.x - view.x, block.y - view.y, block.width, block.height)
+      break
+    case 'C':
+      drawImage('cloud', block.x - view.x, block.y - view.y, block.width, block.height)
+      break
+    default:
+      // DO NOTHING NOW, EMPTY SPACE
+      break
+    }
+  }
 
   const renderView = (parsedMap, view) => {
-    console.log(parsedMap, view)
     parsedMap.forEach(block => {
-      if (utils.intersect(block, view)) {
+      if (utils.viewIntersect(block, view)) {
         renderBlock(block, view)
       }
     })
@@ -116,8 +159,7 @@ const Canvas = (utils = Utils()) => {
   }
 
   const renderBackground = () => {
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    drawImage('background', 0, 0, canvas.width, canvas.height)
   }
 
   const getDimensions = () => {
@@ -160,4 +202,17 @@ const initApp = () => {
   // document.getElementById('world').addEventListener('mousemove', e => console.log(e.clientX))
 }
 
-window.onload = initApp
+window.onload = () => {
+
+  assets.map(asset => {
+    const image = new Image()
+    image.src = asset.uri
+    image.id = asset.id
+    image.style.display = 'none'
+    document.body.appendChild(image)
+  })
+
+  // Hack to wait for all images to be loaded :-X
+  setTimeout(initApp, 500)
+}
+
